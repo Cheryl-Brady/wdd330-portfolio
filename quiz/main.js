ada2f413450779b2143379fe2f845e455fd03ad3
Cheryl Brady Account <cbrady80@byui.edu>
2021-01-17 15:43:49 -0700

    Organizing and still working on week 2 files

--- quiz/main.js
+++ quiz/main.js
@@ -1,2 +1,30 @@
+// THIS GOES WITH QUIZ NINJA!
+
 alert('Welcome to Quiz Ninja!');
 
+const quiz = [
+    ["What is Superman's real name?", "Clark Kent"],
+    ["What is Wonder Woman's real name?", "Diana Prince"],
+    ["What is Batman's real name?", "Bruce Wayne"]
+];
+
+let score = 0; //initialize score
+
+//const question = "What is Superman's real name?"
+//const answer = prompt(question);
+
+// example of a FOR-OF loop
+for(const [question, answer] of quiz){
+    const response = prompt(question);
+    if(response === answer) {
+        alert('Correct!');
+        score++;
+    } else{
+        alert(`Wrong. The correct answer was ${answer}.`);
+    }
+}
+
+// !! REMEMBER TO USE BACKTICKS ` FOR STRINGS WITH STRING LITTERALS IN THEM!!!
+
+// At the end of the game, report the player's score
+alert(`Game Over.  You scored ${score} point${score !== 1 ? "s" : ""}.`);
---
+++ week2/w2code.js
@@ -0,0 +1,27 @@
+// WEEK 2 PRACTICE CODE
+
+// Function Declarations
+function hello(){
+    console.log('Hello World!');
+}
+        
+// Function Expressions - anonymous function
+const goodbye = function(){
+    console.log('Goodbye World!');
+}
+
+// Function Expressions - a named function
+const goodbye = function bye(){
+    console.log('Bye-bye!');
+}
+
+// Return Values
+function howdy(){
+    return 'Howdy World!';
+}
+
+// Variable numbers of arguments - the arguments variable
+function arguments(){
+    return arguments;
+}
+
