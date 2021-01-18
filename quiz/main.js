e421903ec4423505c0040873bf525332ba38f151
Cheryl Brady Account <cbrady80@byui.edu>
2021-01-17 21:23:26 -0700

    Finishing up week 2!

--- quiz/main.js
+++ quiz/main.js
@@ -1,30 +1,41 @@
 // THIS GOES WITH QUIZ NINJA!
 
-alert('Welcome to Quiz Ninja!');
-
 const quiz = [
     ["What is Superman's real name?", "Clark Kent"],
     ["What is Wonder Woman's real name?", "Diana Prince"],
     ["What is Batman's real name?", "Bruce Wayne"]
 ];
 
-let score = 0; //initialize score
+function start(quiz){
+    //initialize score
+    let score = 0; 
 
-//const question = "What is Superman's real name?"
-//const answer = prompt(question);
+    // main game loop (example of a FOR-OF loop)
+    for(const [question, answer] of quiz){
+        const response = ask(question);
+        check(response, answer);
+    }
 
-// example of a FOR-OF loop
-for(const [question, answer] of quiz){
-    const response = prompt(question);
-    if(response === answer) {
-        alert('Correct!');
-        score++;
-    } else{
-        alert(`Wrong. The correct answer was ${answer}.`);
+    gameOver();
+
+    // function declarations
+    function ask(question){
+        return prompt(question);
+    }
+
+    // !! REMEMBER TO USE BACKTICKS ` FOR STRINGS WITH STRING LITTERALS IN THEM!!!
+
+    function check(response, answer){
+        if(response === answer){
+            alert('Correct!');
+            score++;
+        } else{
+            alert(`Wrong, the correct answer is ${answer}.`);
+        }
+    }
+
+    function gameOver(){
+        alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}.`);
     }
 }
-
-// !! REMEMBER TO USE BACKTICKS ` FOR STRINGS WITH STRING LITTERALS IN THEM!!!
-
-// At the end of the game, report the player's score
-alert(`Game Over.  You scored ${score} point${score !== 1 ? "s" : ""}.`);
+start(quiz);
--- week2/w2code.js
+++ week2/w2code.js
@@ -25,3 +25,42 @@ function arguments(){
     return arguments;
 }
 
+// A better option is to use the rest operator
+// (The actual operator is the 3 dots)
+function rest(...args){
+    for(arg of args){
+        console.log(arg);
+    }
+}
+
+// Example mean function
+function mean(...values){
+    let total = 0;
+    for(const value of values){
+        total += value;
+    }
+    return total/values.length;
+}
+
+// Default parameters examples
+function hello(name='World'){
+    console.log(`Hello ${name}!`);
+}
+
+// If mixing a default parameter with a non-default,
+// non-default parameters ALWAYS go first!
+function discount(price, amount=10){
+    return price*(100-amount)/100;
+}
+
+// Arrow functions
+const square = x => x * x; //single parameters do not need ()
+const add = (x, y) => x + y; //multi parameters need to be in ()
+const hello = () => alert('Hello World!'); //no parameters need empty ()
+
+// CALLBACKS - a function that is passed as an argument to another function
+// the callback is provided as a paramenter and then invoked inside the body.
+function sing(song, callback){
+    console.log(`I'm singing along to ${song}.`);
+    callback();
+}
