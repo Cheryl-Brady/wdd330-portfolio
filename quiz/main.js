2cf517ae2ee9836e9aeb73764177aae1061afe49
Cheryl Brady Account <cbrady80@byui.edu>
2021-01-23 17:42:04 -0700

    Finished up week 3

--- quiz/main.js
+++ quiz/main.js
@@ -1,41 +1,106 @@
 // THIS GOES WITH QUIZ NINJA!
 
 const quiz = [
-    ["What is Superman's real name?", "Clark Kent"],
-    ["What is Wonder Woman's real name?", "Diana Prince"],
-    ["What is Batman's real name?", "Bruce Wayne"]
+    { name: "Superman", realName: "Clark Kent" },
+    { name: "Wonder Woman", realName: "Diana Prince" },
+    { name: "Batman", realName: "Bruce Wayne" }
 ];
 
-function start(quiz){
-    //initialize score
-    let score = 0; 
-
-    // main game loop (example of a FOR-OF loop)
-    for(const [question, answer] of quiz){
-        const response = ask(question);
-        check(response, answer);
-    }
-
-    gameOver();
-
-    // function declarations
-    function ask(question){
-        return prompt(question);
-    }
-
-    // !! REMEMBER TO USE BACKTICKS ` FOR STRINGS WITH STRING LITTERALS IN THEM!!!
-
-    function check(response, answer){
-        if(response === answer){
-            alert('Correct!');
-            score++;
-        } else{
-            alert(`Wrong, the correct answer is ${answer}.`);
+const game = {
+    start(quiz){
+        this.questions = [...quiz];
+        this.score = 0;
+        //new main game loop
+        for(const question of this.questions){
+            this.question = question;
+            this.ask();
         }
-    }
-
-    function gameOver(){
-        alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}.`);
+        //end of main game loop
+        this.gameOver;
+        view.hide(view.start);
+    },
+    ask(){
+        const question = `What is ${this.question.name}'s real name?`;
+        view.render(view.question, question);
+        const response = prompt(question);
+        this.check(response);
+    },
+    check(response){
+        const answer = this.question.realName;
+        if(response === answer){
+            view.render(view.result,'Correct!',{'class':'correct'});
+            alert('Correct!');
+            this.score++;
+            view.render(view.score, this.score);
+        } else {
+            view.render(view.result,`Wrong. The correct answer is ${answer}`, {'class':'wrong'});
+            alert(`Wrong. The correct answer is ${answer}.`);
+        }
+    },
+    gameOver(){
+        view.render(view.info,`Game Over.  You scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+        view.show(view.start);
     }
 }
-start(quiz);
+
+//Chapter 6 - View Object
+const view = {
+    score: document.querySelector('#score strong'),
+    question: document.getElementById('question'),
+    result: document.getElementById('result'),
+    info: document.getElementById('info'),
+    start: document.getElementById('start'),
+    render(target,content,attributes) {
+        for(const key in attributes) {
+            target.setAttribute(key, attributes[key]);
+        }
+        target.innerHTML = content;
+    
+    },
+    show(element){
+        element.style.display = 'block';
+    },
+    hide(element){
+        element.style.display = 'none';
+    }
+};
+
+//Chapter 7 - adding an event listener
+view.start.addEventListener('click', ()=> game.start(quiz), false);
+
+
+// WEEK 2 QUIZ NINJA CODE
+// function start(quiz){
+//     //initialize score
+//     let score = 0; 
+
+//     // main game loop (example of a FOR-OF loop)
+//     for(const [question, answer] of quiz){
+//         const response = ask(question);
+//         check(response, answer);
+//     }
+
+//     gameOver();
+
+//     // function declarations
+//     function ask(question){
+//         return prompt(question);
+//     }
+
+//     // !! REMEMBER TO USE BACKTICKS ` FOR STRINGS WITH STRING LITTERALS IN THEM!!!
+
+//     function check(response, answer){
+//         if(response === answer){
+//             alert('Correct!');
+//             score++;
+//         } else{
+//             alert(`Wrong, the correct answer is ${answer}.`);
+//         }
+//     }
+
+//     function gameOver(){
+//         alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}.`);
+//     }
+// }
+
+// start(quiz);
--- week3/w3code.js
+++ week3/w3code.js
@@ -1,2 +1,102 @@
 // WEEK 3 PRACTICE CODE
 
+//Create an object:
+let user = {
+    name: "John",
+    age: 30
+};
+
+//Create a method for the object:
+user.sayHi = function() {
+    console.log("Hello!");
+}
+user.sayHi();
+
+//shorthand example:
+user = {
+    sayHi() {
+        console.log("Hi!");
+    }
+};
+user.sayHi();
+
+//To access the object, a method can use the 'this' keyword.
+// The value of 'this' is the object "before dot", the one used to call the method.
+let user = {
+    name: "Cheryl",
+    age: 40,
+
+    sayHi() {
+        console.log(this.name);
+    }
+};
+user.sayHi();
+
+//The value of 'this' is evaluated during run-time, depending on the context.
+//Example of the same function assigned to 2 different objects:
+let user = { name: "Brooke"};
+let admin = { name: "Kris"};
+
+function sayHello() {
+    console.log( this.name );
+}
+
+//same function used
+//the ".f accesses the method"
+user.f = sayHello;
+admin.f = sayHello;
+
+//These calls have a different value for 'this'
+admin.f();
+user.f();
+
+/* Create an object calculator with 3 methods:
+    - read() prompts for 2 values and saves them as object properties.
+    - sum() returns the sum of saved values.
+    - mul() multiplies saved values and returns the result. */
+let calculator = {
+
+    sum() {
+        return this.a + this.b;
+    },
+
+    mul() {
+        return this.a * this.b;
+    },
+
+    read() {
+        this.a = +prompt('a?', 0);
+        this.b = +prompt('b?', 0);
+    }
+};
+calculator.read();
+console.log = calculator.sum();
+console.log = calculator.mul();  //GETTING AN ERROR "PROMPT IS NOT DEFINED ON this.a = prompt"???
+
+//Testing out array iteration methods:
+//forEach
+[1, 2, 3].forEach(function (item, index) {
+    console.log(item, index);
+  });
+
+//map
+const three = [1, 2, 3];
+const doubled = three.map(function (item) {
+  return item * 2;
+});
+console.log(three === doubled, doubled);
+
+//more practice with objects and 'this'
+const dice = {
+    sides: 6,
+    roll() {
+        return Math.floor(this.sides * Math.random()) + 1;
+    }
+}
+console.log(dice.roll());
+
+//RegExp examples
+const pattern = /[Jj][aeiou]v[aeiou]/;
+console.log(pattern.test('JavaScript'));
+console.log(pattern.test('jive'));
+console.log(pattern.test('hello'));
