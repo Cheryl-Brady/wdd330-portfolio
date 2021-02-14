7a6e3ba146f578022fd4e9b448a76ed840a38b23
Cheryl Brady Account <cbrady80@byui.edu>
2021-02-13 17:41:16 -0700

    Working on the app

--- week6/todo-app/js/ls.js
+++ week6/todo-app/js/ls.js
@@ -1,3 +1,30 @@
+function saveTodo(todo) {
+        const toDoList = getTodoList();
+
+        toDoList.push(todo);
+        localStorage.setItem('toDoList', JSON.stringify(toDoList));
+}
+
+function deleteTodo(id) {
+    const toDoList = getTodoList();
+
+    const updatedTodos = toDoList.filter(todo => todo.id != id)
+    localStorage.setItem('toDoList', JSON.stringify(updatedTodos));
+}
+
+function getTodoList() {
+    const todoListString = localStorage.getItem('toDoList');
+    let toDoList = [];
+
+    if(todoListString) {
+        toDoList = JSON.parse(todoListString);
+    }
+
+    return toDoList;
+}
+
 export default {
-    
+    saveTodo,
+    deleteTodo,
+    getTodoList
 }
